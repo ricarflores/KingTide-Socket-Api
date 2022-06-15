a4c86f4da47cc13b1bd19737b028241940eff345
Ricardo Flores Ramos <ricardofloresra95@gmail.com>
2022-06-15 16:25:08 -0500

    add in git ignore

--- build/models/privateRoom.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const crypto_1 = require("crypto");
-const PrivateRoomSchema = new mongoose_1.Schema({
-    users: [String],
-    idTalk: { type: String, default: (0, crypto_1.randomBytes)(16).toString("hex") }
-}, { collection: 'Rooms' });
-exports.default = (0, mongoose_1.model)('Rooms', PrivateRoomSchema);
